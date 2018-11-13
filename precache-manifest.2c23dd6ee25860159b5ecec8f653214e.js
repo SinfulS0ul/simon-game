6fc5d5ec2c14f56c856e205670c7ce5fcccb8e3e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-13 16:32:49 +0000

    Deploy SinfulS0ul/simon-game to github.com/SinfulS0ul/simon-game.git:gh-pages

---
+++ precache-manifest.2c23dd6ee25860159b5ecec8f653214e.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "83d7c47d05b4ecad0011",
+    "url": "main.js"
+  },
+  {
+    "revision": "83d7c47d05b4ecad0011",
+    "url": "main.css"
+  },
+  {
+    "revision": "05055467e775dd6d8103ed4dcbf6ace3",
+    "url": "index.html"
+  }
+];
