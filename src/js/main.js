2ebe0404ce972833bdfcc0714d96582b1fbe65e6
SinfulS0ul <pasichnikmaks@gmail.com>
2018-11-14 18:56:12 +0200

    sw update

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js');
+  navigator.serviceWorker.register('/simon-game/sw.js');
 }
 
 const $red = document.querySelector('.red');
